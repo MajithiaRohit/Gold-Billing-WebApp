8c0678d676f96e4567931cc6e76551ba9a3ef9ca
RohitMajithia <majithiarohit2001@gmail.com>
2025-02-07 13:55:40 +0530

    rate cut and aamount added

---
+++ Gold Billing Web-App/Controllers/RateCutTransectionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gold_Billing_Web_App.Controllers
+{
+    public class RateCutTransectionController : Controller
+    {
+        public IActionResult GenrateRateCutVoucher()
+        {
+            return View();
+        }
+    }
+}
